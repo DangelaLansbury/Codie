f01f4636a77907b61ba83b304f7452ca80ea9e90
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2025-02-17 02:58:38 -0500

    Add instruction text

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { CodonSelector } from './components/BaseSelector';
 function App() {
   return (
     <>
+      <p>Pick 3 bases to translate codon to amino acid.</p>
       <CodonSelector />
     </>
   );
