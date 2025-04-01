82b3f1095d72e2d669190a336d569e78e40027f4
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2025-03-31 21:58:10 -0400

    Migrate from styled-components to css modules

--- src/components/CompareAAs.tsx
+++ src/components/CompareAAs.tsx
@@ -1,87 +1,27 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styles from '../css/CompareAAs.module.css';
 import { AminoAcidData, getFoldEffect } from './AAData';
 import { CodonSelector } from './BaseSelector';
-
-const MainContainer = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  height: 100vh;
-  justify-content: center;
-  padding: 1rem 0;
-  width: clamp(20rem, 90vw, 24rem);
-`;
-
-const SelectorsContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 1rem;
-  width: 100%;
-  position: relative;
-  &::before {
-    content: '';
-    display: block;
-    width: 1px;
-    height: 100%;
-    background-color: var(--ink-100);
-    position: absolute;
-    left: 50%;
-  }
-`;
-
-const DetailsContainer = styled.div`
-  align-items: top;
-  display: flex;
-  flex-direction: row;
-  gap: 1rem;
-  justify-content: center;
-  width: 100%;
-`;
-
-const ContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  width: 100%;
-  &.top {
-    justify-content: flex-end;
-  }
-  &.bottom {
-    justify-content: flex-start;
-  }
-`;
-
-const FoldEffectContainer = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  justify-content: flex-start;
-  padding: 1.5rem 0;
-  width: 100%;
-`;
 
 export const CompareAAs: React.FC = () => {
   const [wildtype, setWildtype] = useState<AminoAcidData | null>(null);
   const [mutant, setMutant] = useState<AminoAcidData | null>(null);
 
   return (
-    <MainContainer>
-      <ContentWrapper className="top">
-        <SelectorsContainer>
+    <div className={styles.mainContainer}>
+      <div className={`${styles.contentWrapper} ${styles.top}`}>
+        <div className={styles.selectorsContainer}>
           <CodonSelector codon={wildtype} setCodon={setWildtype} />
           <CodonSelector codon={mutant} setCodon={setMutant} isMutant={true} />
-        </SelectorsContainer>
-      </ContentWrapper>
-      <ContentWrapper className="bottom">
-        <DetailsContainer>
+        </div>
+      </div>
+      <div className={`${styles.contentWrapper} ${styles.bottom}`}>
+        <div className={styles.detailsContainer}>
           {wildtype && mutant && wildtype.name === mutant.name ? (
-            <FoldEffectContainer>{getFoldEffect(wildtype.details)}</FoldEffectContainer>
+            <div className={styles.foldEffectContainer}>{getFoldEffect(wildtype.details)}</div>
           ) : (
             <>
-              <FoldEffectContainer>
+              <div className={styles.foldEffectContainer}>
                 {wildtype ? (
                   <>
                     <span>{getFoldEffect(wildtype.details)}</span>
@@ -89,8 +29,8 @@ export const CompareAAs: React.FC = () => {
                 ) : (
                   <span>wildtype</span>
                 )}
-              </FoldEffectContainer>
-              <FoldEffectContainer>
+              </div>
+              <div className={styles.foldEffectContainer}>
                 {mutant ? (
                   <>
                     <span>{getFoldEffect(mutant.details)}</span>
@@ -98,11 +38,11 @@ export const CompareAAs: React.FC = () => {
                 ) : (
                   <span>mutant</span>
                 )}
-              </FoldEffectContainer>
+              </div>
             </>
           )}
-        </DetailsContainer>
-      </ContentWrapper>
-    </MainContainer>
+        </div>
+      </div>
+    </div>
   );
 };
