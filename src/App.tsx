21aafa88446135dc3ded77df5260bf820ae7a243
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2025-04-08 21:03:55 -0400

    Gelica

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
 import { CompareAAs } from './components/CompareAAs';
-import Blob from './components/Blob';
 
 function App() {
   return (
     <>
-      <Blob />
       <CompareAAs />
     </>
   );
